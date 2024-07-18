15f4b420c11ac08621002e92ceb92ddb740f0f6f
NAVEEN SENTHILNATHAN <capgemini.senthilnathan@mercedes-benz.com>
2024-07-18 06:10:19 +0530

    commit for future reference

--- src/Builder/Main.java
+++ src/Builder/Main.java
@@ -1,7 +1,7 @@
 package Builder;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Student s = Student.getBuilder().id(1).name("Kohli").build();
         System.out.println(s.id);
         System.out.println(s.name);
