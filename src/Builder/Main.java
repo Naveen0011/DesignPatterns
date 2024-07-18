4275bb35068671b003799e6836941e0116d58f13
NAVEEN SENTHILNATHAN <capgemini.senthilnathan@mercedes-benz.com>
2024-07-18 06:09:31 +0530

    commit for future reference

--- src/Builder/Main.java
+++ src/Builder/Main.java
@@ -2,7 +2,8 @@ package Builder;
 
 public class Main {
     public static void main(String args[]){
-        Student s = Student.getBuilder().build();
+        Student s = Student.getBuilder().id(1).name("Kohli").build();
         System.out.println(s.id);
+        System.out.println(s.name);
     }
 }
--- src/Singleton/Main.java
+++ src/Singleton/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Singleton s = Singleton.getInstance();
         System.out.println(s.random);
+        Singleton s1 = Singleton.getInstance();
+        System.out.println(s1.random);
     }
 }
